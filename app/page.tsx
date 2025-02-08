308385b6527d63f7d23a321777cc5acb4ad87d42
Azekowka <gabjtov@gmail.com>
2025-02-08 10:34:27 +0500

    landing page added

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Header from '@/components/landing/header';
+import CTA from '@/components/landing/cta';
+import Clouds from '@/components/landing/clouds';
+import Hero from '@/components/landing/hero';
+import Bento from '@/components/landing/bento';
+import Footer from '@/components/landing/footer';
+import News from '@/components/landing/news';
+
+export default function Home() {
+    return (
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <CTA />
+        <Hero />
+        <Bento />
+        <Clouds />
+        <News />
+        <Footer />
+      </div>
+    );
+  }
---
+++ components/landing/header.tsx
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <header className="bg-white">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1 lg:justify-start">
+        <img
+              alt=""
+              src="https://akb.nis.edu.kz/img/ornament.png"
+              className="h-8 w-auto mr-4"
+            />
+            <span className="text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-xl">Единый профиль учащегося</span>
+        </div>
+        <div className="flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-3xl font-semibold tracking-tight text-balance text-gray-900 sm:text-xl">
+            Личный кабинет <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
